f127b50af867349645af3c9553d554192edeac7c
Xray <xraydylan@gmail.com>
2024-02-14 22:46:21 +0100

    Url update

--- tooltips.js
+++ tooltips.js
@@ -7,8 +7,8 @@
 // @match        https://apex.prosperousuniverse.com/
 // @grant        none
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js
-// @downloadURL  https://raw.githubusercontent.com/Xraydylan/PrUn_Tooltips_KAWA/main/tooltips.js
-// @updateURL    https://raw.githubusercontent.com/Xraydylan/PrUn_Tooltips_KAWA/main/tooltips.js
+// @downloadURL  https://raw.githubusercontent.com/Xraydylan/PrUn_Tooltips_KAWA/Only-KAWA/tooltips.js
+// @updateURL    https://raw.githubusercontent.com/Xraydylan/PrUn_Tooltips_KAWA/Only-KAWA/tooltips.js
 // ==/UserScript==
 
 let $ = jQuery;
